038197d26f73ef1bc88f48a821e58345546c6313
Codearick <rohan.gautam650@gmail.com>
2025-04-24 01:22:56 +0530

    add intros

---
+++ intro.js
@@ -0,0 +1,12 @@
+var user = {
+    name: "Rohan Gautam",
+    age: 23,
+    email: "rohan.gautam@email.com",
+    hobbies: ["reading", "gaming", "coding"],
+    address: {
+        city: "New York",
+        state: "NY",
+        country: "USA"
+    }
+};
+console.log(user);
---
+++ intro.ts
@@ -0,0 +1,13 @@
+const user = {
+    name: "Rohan Gautam",
+    age: 23, 
+    email: "rohan.gautam@email.com",
+    hobbies: ["reading", "gaming", "coding"],
+    address: {
+        city: "New York",
+        state: "NY",
+        country: "USA"
+    }
+}
+
+console.log(user);
